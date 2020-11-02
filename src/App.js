e7a1813cb66067879b3512f670eebf1d4d05a382
barisenak <mariya1999-7@mail.ru>
2020-11-02 16:13:11 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    italic: false,
+  };
+
+  cursive = () => {
+    this.state.italic === false
+      ? this.setState({ italic: true })
+      : this.setState({ italic: false });
+  };
+
+  render() {
+    return (
+      <p className={this.state.italic ? "text" : ""} onClick={this.cursive}>
+        lorem ipsum
+      </p>
+    );
+  }
 }
 
 export default App;
